faa282ae085ad96d096869172b3ca3f43e5fc1c7
satoshi <satoshi@kawagucchi.net>
2024-10-11 14:13:38 +0900

    20241009

--- library-dev/project_1/src/model/factory/condition_evaluator.py
+++ library-dev/project_1/src/model/factory/condition_evaluator.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import re
 
 from src.lib.common_utils.ibr_enums import LogLevel
 from src.lib.common_utils.ibr_decorator_config import with_config
 from src.lib.validator_utils.ibr_decision_table_validator import DT
 
+
+class ReExceptionError(Exception):
+    pass
+    
 @with_config
 class ConditionEvaluator:
     def __init__(self, config: dict|None = None):
@@ -26,19 +31,44 @@ class ConditionEvaluator:
         return 'DataFrameEditorDefault'
 
     def _check_condition(self, value: str|int, condition: str|int) -> bool:
-        if pd.isna(condition) or condition == "any":
-            return True
-
-        if pd.isna(value):
+        if pd.isna(condition) :
             return False
 
+        if condition == "any":
+            return True
+
         if isinstance(condition, str):
-            if condition in self.dt_functions:
-                return self._check_dt_function(value, condition)
             if "," in condition:
                 return self._check_or_condition(value, condition)
+            if condition in self.dt_functions:
+                return self._check_dt_function(value, condition)
+            if self.is_regex(condition):
+                return self._check_regex(value, condition)
 
         return str(value) == str(condition)
+
+    def is_regex(self, condition: str) -> bool:
+        if not condition:   # 空の条件の場合は False を返す
+            return False
+        try:
+            re.compile(condition)
+        except re.error:
+            return False
+        except ReExceptionError:
+            return False
+        else:
+            return True
+
+    def _check_regex(self, value: str|int, condition: str) -> bool:
+        if not condition:  # 空の正規表現パターンの場合は False を返す
+            return False
+
+        if isinstance(value, str|int):
+            value = str(value)
+            match = re.match(condition, value)
+            self.log_msg(f'Regex {condition}: match result: {bool(match)}', LogLevel.DEBUG)
+            return bool(match)
+        return False
 
     def _check_dt_function(self, value: str|int, condition: str) -> bool:
         result = self.dt_functions[condition](value)
@@ -46,15 +76,17 @@ class ConditionEvaluator:
         return result
 
     def _check_or_condition(self, value: str|int, condition: str) -> bool:
+        if not condition:  # 空の条件文字列の場合は False を返す
+            return False
+
         conditions = [c.strip() for c in condition.split(',')]
         self.log_msg(f"conditions {conditions}", LogLevel.INFO)
 
         for c in conditions:
-            if pd.isna(c) or c == "any":
+            if c in self.dt_functions and self.dt_functions[c](value):
                 return True
-            if c in self.dt_functions:
-                if self.dt_functions[c](value):
-                    return True
-            elif str(value) == str(c):
+            if self.is_regex(c) and self._check_regex(value, c):
+                return True
+            if str(value) == str(c):
                 return True
         return False
